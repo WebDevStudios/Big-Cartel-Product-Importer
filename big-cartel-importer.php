0721f71a0499b49193ba4d6a3ec758aaaa8e1e23
Michael Beckwith <tw2113@gmail.com>
2016-11-18 18:58:55 -0600

    load textdomain for WP.org language pack usage

--- big-cartel-importer.php
+++ big-cartel-importer.php
@@ -7,10 +7,14 @@ Version: 1.1.0
 Author: Pluginize
 Author URI: http://www.pluginize.com
 License: GPLv2
+Text Domain: big-cartel-product-importer
 */
 
 function wdsbc_load_importer() {
 	$plugin_dir_path = dirname( __FILE__ );
+
+	load_plugin_textdomain( 'big-cartel-product-importer' );
+
 	require_once $plugin_dir_path . '/classes/class-wdsbc-import.php';
 	require_once $plugin_dir_path . '/includes/content-types.php';
 	require_once $plugin_dir_path . '/includes/metaboxes.php';
