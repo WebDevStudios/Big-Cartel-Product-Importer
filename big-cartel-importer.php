c304e3c3e60deaa01b1947c899f149e8354823e5
Michael Beckwith <tw2113@gmail.com>
2018-01-03 01:38:31 -0600

    cron helper function to help michael

--- big-cartel-importer.php
+++ big-cartel-importer.php
@@ -24,3 +24,37 @@ function wdsbc_load_importer() {
 	require_once $plugin_dir_path . '/includes/settings.php';
 }
 add_action( 'plugins_loaded', 'wdsbc_load_importer' );
+
+function cron_spawn() {
+	global $wp_version;
+	if ( defined( 'DISABLE_WP_CRON' ) && DISABLE_WP_CRON ) {
+		// translators: placeholder will be a timestamp for the current time.
+		return sprintf( 'The DISABLE_WP_CRON constant is set to true as of %s. WP-Cron is disabled and will not run.', current_time( 'm/d/Y g:i:s a' ) );
+	}
+	if ( defined( 'ALTERNATE_WP_CRON' ) && ALTERNATE_WP_CRON ) {
+		// translators: placeholder will be a timestamp for the current time.
+		return sprintf( 'The ALTERNATE_WP_CRON constant is set to true as of %s.  This plugin cannot determine the status of your WP-Cron system.', current_time( 'm/d/Y g:i:s a' ) );
+	}
+	$sslverify     = version_compare( $wp_version, 4.0, '<' );
+	$doing_wp_cron = sprintf( '%.22F', microtime( true ) );
+	$cron_request = apply_filters( 'cron_request', array(
+		'url'  => site_url( 'wp-cron.php?doing_wp_cron=' . $doing_wp_cron ),
+		'key'  => $doing_wp_cron,
+		'args' => array(
+			'timeout'   => 3,
+			'blocking'  => true,
+			'sslverify' => apply_filters( 'https_local_ssl_verify', $sslverify ),
+		),
+	) );
+	$cron_request['args']['blocking'] = true;
+	$result = wp_remote_post( $cron_request['url'], $cron_request['args'] );
+	if ( is_wp_error( $result ) ) {
+		return $result;
+	} elseif ( wp_remote_retrieve_response_code( $result ) >= 300 ) {
+		return sprintf(
+			'Unexpected HTTP response code: %s',
+			intval( wp_remote_retrieve_response_code( $result ) )
+		);
+	}
+	return 'Cron spawn ok';
+}
