c2f1c179183f606e0b1a1a005bab82bd20f016e0
Michael Beckwith <tw2113@gmail.com>
2018-01-03 00:43:33 -0600

    better plugin head comment

--- big-cartel-importer.php
+++ big-cartel-importer.php
@@ -1,14 +1,14 @@
 <?php
-/*
-Plugin Name: Big Cartel Product Importer
-Plugin URI: http://www.pluginize.com
-Description: Import your products from Big Cartel to a Product custom post type in WordPress.
-Version: 1.1.0
-Author: Pluginize
-Author URI: http://www.pluginize.com
-License: GPLv2
-Text Domain: big-cartel-product-importer
-*/
+/**
+ * Plugin Name: Big Cartel Product Importer
+ * Plugin URI: http://www.pluginize.com
+ * Description: Import your products from Big Cartel to a Product custom post type in WordPress.
+ * Version: 1.1.0
+ * Author: Pluginize
+ * Author URI: http://www.pluginize.com
+ * License: GPLv2+
+ * Text Domain: big-cartel-product-importer
+ */
 
 function wdsbc_load_importer() {
 	$plugin_dir_path = dirname( __FILE__ );
